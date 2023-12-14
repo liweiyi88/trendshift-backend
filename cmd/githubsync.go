a60a288c811f3d640064c486648ece0e061b915a
liweiyi88 <weiyi.li713@gmail.com>
2023-12-14 17:01:51 +1100

    provides ability to filter sync records by hour and days

--- cmd/githubsync.go
+++ cmd/githubsync.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -22,18 +25,15 @@ import (
 var start string
 var end string
 var limit int
-var recurring bool
 
 // If run as cronjob, a suggested command to avoid sending too many requests to GitHub is
-// `sync [repository|developer] --recurring=true --limit=500` and run it hourly.
-// This makes sure that we update the latest repositories/developers details once a week.
+// `sync [repository|developer] --end=-2d --limit=500` and run it hourly.
 func init() {
 	rootCmd.AddCommand(gihtubSyncCmd)
 
 	gihtubSyncCmd.Flags().StringVarP(&start, "start", "s", "", "--start \"2023-01-06 14:35:00\" ")
-	gihtubSyncCmd.Flags().StringVarP(&end, "end", "e", "", "--end \"2023-10-06 14:35:00\" ")
+	gihtubSyncCmd.Flags().StringVarP(&end, "end", "e", "", "--end \"2023-10-06 14:35:00\" or --end=-2d, --end=2h, `d` for days, `h` for hours ")
 	gihtubSyncCmd.Flags().IntVarP(&limit, "limit", "l", 0, "--limit=100")
-	gihtubSyncCmd.Flags().BoolVarP(&recurring, "recurring", "r", false, "--recurring=true")
 }
 
 var gihtubSyncCmd = &cobra.Command{
@@ -69,20 +69,11 @@ var gihtubSyncCmd = &cobra.Command{
 			}
 		}
 
-		// When running as cronjob, we only update entries whose last updated day is before the start of this week.
-		// This avoid sending too much requests to GitHub.
-		if recurring {
-			startDayOfWeek := startDayOfWeek(time.Now())
-			end = startDayOfWeek.Format("2006-01-02 15:04:05")
-		} else {
-			if end != "" {
-				_, err := time.Parse("2006-01-02 15:04:05", end)
-				if err != nil {
-					slog.Error("failed to parse end time", slog.Any("error", err))
-					sentry.CaptureException(err)
-					return
-				}
-			}
+		endDateTime, err := parseEndDateTimeOption(end)
+		if err != nil {
+			slog.Error("failed to parse end date time", slog.Any("error", err))
+			sentry.CaptureException(err)
+			return
 		}
 
 		appSignal := make(chan os.Signal, 3)
@@ -97,7 +88,7 @@ var gihtubSyncCmd = &cobra.Command{
 		repositoryRepo := model.NewGhRepositoryRepo(db, qb)
 		developerRepo := model.NewDeveloperRepo(db, qb)
 		handler := github.NewSyncHandler(db, repositoryRepo, developerRepo, gh)
-		err := handler.Handle(ctx, action, opt.Start(start), opt.End(end), opt.Limit(limit))
+		err = handler.Handle(ctx, action, opt.Start(start), opt.End(endDateTime), opt.Limit(limit))
 
 		if err != nil {
 			slog.Error("failed to handle sync action", slog.Any("error", err))
@@ -106,13 +97,41 @@ var gihtubSyncCmd = &cobra.Command{
 	},
 }
 
-// Get the start day of the week. e.g. 2023-12-04 00:00:00
-func startDayOfWeek(tm time.Time) time.Time {
-	weekday := time.Duration(tm.Weekday())
-	if weekday == 0 {
-		weekday = 7
+func parseEndDateTimeOption(end string) (string, error) {
+	end = strings.TrimSpace(end)
+
+	if end == "" {
+		return "", nil
 	}
-	year, month, day := tm.Date()
-	currentZeroDay := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
-	return currentZeroDay.Add(-1 * (weekday - 1) * 24 * time.Hour)
+
+	var unit string
+	var numberPart string
+
+	if strings.HasSuffix(end, "d") {
+		unit = "d"
+		numberPart, _ = strings.CutSuffix(end, "d")
+	} else if strings.HasSuffix(end, "h") {
+		unit = "h"
+		numberPart, _ = strings.CutSuffix(end, "h")
+	}
+
+	number, err := strconv.Atoi(numberPart)
+	if err == nil && unit != "" {
+		now := time.Now()
+
+		if unit == "d" {
+			endTime := now.Add(time.Duration(number) * 24 * time.Hour)
+			end = endTime.Format("2006-01-02 15:04:05")
+			return end, nil
+		} else if unit == "h" {
+			endTime := now.Add(time.Duration(number) * time.Hour)
+
+			fmt.Println("end time", endTime.Format("2006-01-02 15:04:05"))
+			end = endTime.Format("2006-01-02 15:04:05")
+			return end, nil
+		}
+	}
+
+	_, err = time.Parse("2006-01-02 15:04:05", end)
+	return end, err
 }
---
+++ cmd/githubsync_test.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParseEndOption(t *testing.T) {
+	end := "         "
+	end, err := parseEndDateTimeOption(end)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if end != "" {
+		t.Errorf("expected empty string but got: %s", end)
+	}
+
+	end = "2000-12-31 01:20:59"
+	end, err = parseEndDateTimeOption(end)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if end != "2000-12-31 01:20:59" {
+		t.Errorf("unexpected time string: %s", end)
+	}
+
+	end = "2s"
+	_, err = parseEndDateTimeOption(end)
+	if err == nil {
+		t.Error("expected invalid parse error but got nil")
+	}
+
+	end = "-2d"
+	now := time.Now()
+	end, err = parseEndDateTimeOption(end)
+	if err != nil {
+		t.Error(err)
+	}
+
+	endDate, err := time.Parse("2006-01-02 15:04:05", end)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if now.Add(time.Duration(-2) * 24 * time.Hour).After(endDate) {
+		t.Errorf("unepexted end date: %v", endDate)
+	}
+
+	end = "2h"
+	now = time.Now()
+	end, err = parseEndDateTimeOption(end)
+	if err != nil {
+		t.Error(err)
+	}
+
+	endDate, err = time.Parse("2006-01-02 15:04:05", end)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if now.Add(time.Duration(-2) * time.Hour).After(endDate) {
+		t.Errorf("unepexted end date: %v", endDate)
+	}
+}
