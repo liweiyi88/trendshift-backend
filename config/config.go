e57a3bb630d700fdda939a88ff2e2a0052a57b9e
Julian Li <weiyi.li713@gmail.com>
2025-04-05 17:36:54 +1100

    support kotlin

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ import (
 
 const JWTMaxAge = 60 * time.Minute
 
-var LanguageToScrape = []string{"", "javascript", "python", "go", "java", "php", "c++", "c", "typescript", "ruby", "c#", "rust", "dart", "swift"}
+var LanguageToScrape = []string{"", "javascript", "python", "go", "java", "php", "c++", "c", "typescript", "ruby", "c#", "rust", "dart", "swift", "kotlin"}
 
 var (
 	DatabaseDSN          string
